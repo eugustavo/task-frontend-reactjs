348928166f7a9b0f59a5f531cfac60f80fd4d5c6
Gustavo Pereira de Souza <gustavo.souza@crp.com>
2019-12-18 12:10:12 -0300

    Resolve env example

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,4 +1,6 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import useModal from 'react-hooks-use-modal';
+
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -8,38 +10,40 @@ import { FaPlus, FaCheck, FaEdit, FaTrash } from 'react-icons/fa'
 import './styles.css';
 
 export default function Dashboard({ history }) {
+  // eslint-disable-next-line
+  const [Modal, open, close] = useModal('root');
   const [task, setTask] = useState([]);
 
-  useEffect( () => {
-    async function loadTask(){
+  useEffect(() => {
+    async function loadTask() {
       const user_id = localStorage.getItem('user');
-      const response = await api.get('/dashboard', { headers: { user_id }});
+      const response = await api.get('/dashboard', { headers: { user_id } });
       setTask(response.data);
     }
     loadTask();
     localStorage.setItem('task', '');
   }, [task]);
 
-  async function newTask(){
+  async function newTask() {
     history.push('/new');
   }
 
-  async function checkTask(task_id){
+  async function checkTask(task_id) {
     const user_id = localStorage.getItem('user');
 
-    await api.delete('/task', {headers: {user_id, task_id}});
+    await api.delete('/task', { headers: { user_id, task_id } });
     toast.success('Tarefa concluída!');
   }
 
-  async function editTask(task_id){
+  async function editTask(task_id) {
     localStorage.setItem('task', task_id);
     history.push('/new');
   }
 
-  async function deleteTask(task_id){
+  async function deleteTask(task_id) {
     const user_id = localStorage.getItem('user');
 
-    const response = await api.delete('/task', {headers: {user_id, task_id}});
+    const response = await api.delete('/task', { headers: { user_id, task_id } });
     toast.error(response.data.message);
   }
 
@@ -50,7 +54,7 @@ export default function Dashboard({ history }) {
         <h2>Minhas Tarefas</h2>
         <button type="button" onClick={newTask} >
           <div className="button-icon">
-            <FaPlus color="#ffffff"/>
+            <FaPlus color="#ffffff" />
           </div>
           Nova tarefa
         </button>
@@ -62,9 +66,9 @@ export default function Dashboard({ history }) {
               <strong>{task.title}</strong>
               <span>{task.description}</span>
               <div className="task-icons">
-                <FaCheck  className="check-icon"  onClick={() => checkTask(task._id)} />
-                <FaEdit   className="edit-icon"   onClick={() => editTask(task._id)} />
-                <FaTrash  className="delete-icon" onClick={() => deleteTask(task._id)}/>
+                <FaCheck className="check-icon" onClick={() => checkTask(task._id)} />
+                <FaEdit className="edit-icon" onClick={() => editTask(task._id)} />
+                <FaTrash className="delete-icon" onClick={() => deleteTask(task._id)} />
               </div>
             </li>
           ))}
